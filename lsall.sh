3598353107e113e083b797fc6f4eb636fd3f12ba
rymann22 <64804167+rymann22@users.noreply.github.com>
2022-02-21 10:17:31 -0700

    Update lsall.sh

--- lsall.sh
+++ lsall.sh
@@ -6,13 +6,19 @@ echo ""
 #only lists directories in current folder
 ls -ld */ --color=auto
 echo ""
+echo "Total Directories:" & ls -ld */ --color=auto |wc -l
+echo ""
 echo "=====[ Files ]====="
 echo ""
 #only lists files in current folder (grep -v ignores strings, ^d is for the directories and ^l is for the links)
 ls -l | grep -vE '^d|^l'
 echo ""
+echo "Total Files:" & ls -l | grep -vE '^d|^l' |wc -l
+echo ""
 echo "=====[ Links ]====="
 echo ""
 #only lists links. the awk portion can be left out. I just wanted to clean up the output a little bit
 find ./* -maxdepth 0 -type l -ls |awk '{for(i=3; i<=13; i++) printf $i"  "; print ""}'
+echo ""
+echo "Total Links:" & find ./* -maxdepth 0 -type l -ls |awk '{for(i=3; i<=13; i++) printf $i"  "; print ""}' |wc -l
 echo ""
