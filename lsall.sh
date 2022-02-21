0f39ba483a8cb164a739defe8dffbe8942051e1e
rymann22 <64804167+rymann22@users.noreply.github.com>
2022-02-21 11:25:39 -0700

    Update lsall.sh

--- lsall.sh
+++ lsall.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 echo ""
-echo "=====[ Directories ]====="
+
+for DIRECTORY in $(find ./* -maxdepth 0 -type d -ls | wc -l); do printf "=====[ Directories: $DIRECTORY ]====="'\n';done
 echo ""
 #only lists directories in current folder
 ls -ld */ --color=auto
 echo ""
-echo "Total Directories:" & ls -ld */ |wc -l
-#I had an issue with this listing one of my symbolic links, so below is an alternative...
-#find ./* -maxdepth 0 -type d -ls | wc -l
-echo ""
-echo "=====[ Files ]====="
+
+for FILES in $(ls -l | grep -vE '^d|^l' |wc -l); do printf "=====[ Files: $FILES ]====="'\n';done
 echo ""
 #only lists files in current folder (grep -v ignores strings, ^d is for the directories and ^l is for the links)
 ls -l | grep -vE '^d|^l'
 echo ""
-echo "Total Files:" & ls -l | grep -vE '^d|^l' |wc -l
-echo ""
-echo "=====[ Links ]====="
+
+for LINKS in $(find ./* -maxdepth 0 -type l -ls | wc -l); do printf "=====[ Links: $LINKS ]====="'\n';done
 echo ""
 #only lists links. the awk portion can be left out. I just wanted to clean up the output a little bit
 find ./* -maxdepth 0 -type l -ls |awk '{for(i=3; i<=13; i++) printf $i"  "; print ""}'
 echo ""
-echo "Total Links:" & find ./* -maxdepth 0 -type l -ls |awk '{for(i=3; i<=13; i++) printf $i"  "; print ""}' |wc -l
+
+for TOTAL in $(ls -l |wc -l); do printf "=====[ Total: $TOTAL ]====="'\n';done
 echo ""
