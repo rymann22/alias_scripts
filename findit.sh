f778de65d39a8890390b421f9d29cca98e8821b0
rymann22 <64804167+rymann22@users.noreply.github.com>
2022-02-23 15:27:57 -0700

    Update findit.sh

--- findit.sh
+++ findit.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 #search string in current directory
-find ./* -iname '*'$1'*'
-
-#if you want to ignore all "permission denied" or "operation not permitted" lines
-find ./* -iname '*'$1'*' -print 2>/dev/null
+find ./* -iname '*'$1'*' 2>/dev/null
